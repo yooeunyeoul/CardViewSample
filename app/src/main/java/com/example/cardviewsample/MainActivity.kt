ccdc2fd1f52d741f40ee3af7932b4a79cfae2907
eunyeoul <eunyull102@naver.com>
2020-03-17 10:26:12 +0900

    Jsoup 테스트

--- app/src/main/java/com/example/cardviewsample/MainActivity.kt
+++ app/src/main/java/com/example/cardviewsample/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.example.cardviewsample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.cardviewsample.Dao.LottoDao
+import android.os.StrictMode
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.runBlocking
+import org.jsoup.Jsoup
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val policy =
+            StrictMode.ThreadPolicy.Builder().permitAll().build()
+
+        StrictMode.setThreadPolicy(policy)
 
         tv_select.setOnClickListener {
 
@@ -20,10 +27,23 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+        btn_get_crowling.setOnClickListener {
+            getLottoInfo()
+        }
+
 
     }
 
     fun insertLotto()= runBlocking {
+
+    }
+
+    fun getLottoInfo() {
+        val doc = Jsoup.connect("https://dhlottery.co.kr/gameResult.do?method=byWin&drwNo=897").get()
+
+        val lottoRound = doc.select("div[class=win_result]").select("h4").text()
+        val winningNums = doc.select("div[class=nums]").select("span").eachText()
+
 
     }
 }
