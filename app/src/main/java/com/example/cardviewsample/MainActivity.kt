ea61a2a7b7c821fa68994bcd9821ab849aa137e0
eunyeoul <eunyull102@naver.com>
2020-03-23 17:34:48 +0900

    크롤링 모델 추가 및 람다함수 테스트

--- app/src/main/java/com/example/cardviewsample/MainActivity.kt
+++ app/src/main/java/com/example/cardviewsample/MainActivity.kt
@@ -3,9 +3,11 @@ package com.example.cardviewsample
 import android.os.Bundle
 import android.os.StrictMode
 import androidx.appcompat.app.AppCompatActivity
+import com.orhanobut.logger.Logger
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.runBlocking
 import org.jsoup.Jsoup
+import org.jsoup.nodes.Element
 
 
 class MainActivity : AppCompatActivity() {
@@ -34,16 +36,82 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun insertLotto()= runBlocking {
+    fun insertLotto() = runBlocking {
 
     }
 
     fun getLottoInfo() {
-        val doc = Jsoup.connect("https://dhlottery.co.kr/gameResult.do?method=byWin&drwNo=897").get()
 
-        val lottoRound = doc.select("div[class=win_result]").select("h4").text()
-        val winningNums = doc.select("div[class=nums]").select("span").eachText()
+        for (i in 897..900) {
+
+            val doc =
+                Jsoup.connect("https://dhlottery.co.kr/gameResult.do?method=byWin&drwNo={$i}").get()
+
+            val lottoRound = doc.select("div[class=win_result]").select("h4").text()
+//            val winningNums = doc.select("div[class=nums]").select("span").eachText()
+
+            val resultOfTable = doc.select("div[class=content_wrap content_winnum_645]")
+                .select("table[class=tbl_data tbl_data_col]").select("tr")
+
+//        val numberOne = resultOfTable[1]
+//        val firstWinningResult = numberOne.select("td")
+
+            for (i in 0 until resultOfTable.size) {
+                if (i == 0) {
+                    continue
+                }
+                saveLottoData(resultOfTable[i])
+            }
+
+
+
+
+        }
 
 
     }
+
+    private fun saveLottoData(element: Element) {
+
+        //
+        val winningResult = element.select("td")
+
+        for (i in 0 until winningResult.size) {
+            when (i) {
+//                0 or 4 -> {
+//                    Logger.e("이거 스킵하세요")
+//                }
+
+                // 1등 당첨금액
+                1 -> {
+                    val totalAmountFirstWinning = winningResult[i].text()
+                    Logger.e(totalAmountFirstWinning)
+                }
+
+                // 1등 당첨자 수
+                2 -> {
+                    val totalCountWinningPeople = winningResult[i].text()
+                    Logger.e(totalCountWinningPeople)
+                }
+
+                // 1등 개인 보상
+                3 -> {
+                    val individualWinningReward = winningResult[i].text()
+                    Logger.e(individualWinningReward)
+                }
+
+                // 비고
+                5 -> {
+                    val beego = winningResult[i].text()
+                    Logger.e(beego)
+                }
+
+            }
+
+
+        }
+
+
+    }
+
 }
--- app/src/test/java/com/example/cardviewsample/ExampleUnitTest.kt
+++ app/src/test/java/com/example/cardviewsample/ExampleUnitTest.kt
@@ -14,4 +14,27 @@ class ExampleUnitTest {
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun LamDaCheck() {
+        val strings = listOf<String>("adb,", "def")
+        println(strings.flatMap { it.toList() })
+
+//        val books = listOf(Book("Thursday Next", listOf("Jasper Fforde")), Book("Mort", listOf("Terry Pratchett")), Book("Good Omens", listOf("Terry Pratchett", "Neil Gaiman")))
+
+        val naturanNumbers = generateSequence(0){
+            it+1
+        }
+
+
+
+        val numbersTo100 = naturanNumbers.takeWhile { it<=100 }
+
+        println(numbersTo100.sum())
+
+
+
+    }
+
+
 }
